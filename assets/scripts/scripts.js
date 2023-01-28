a164096f6c800be90ddb6b8ea7655d6a1e4d9c88
omgthegreenranger <das.shaggy@gmail.com>
2023-01-28 14:05:26 -0500

    Just updating with the work.

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,1 +1,11 @@
+let searchBtn = document.querySelector("#searchLoc");
+let searchFormat = document.querySelector("#dropdown-chosen").value;
+let searchString = document.querySelector("#loc-search").value;
 
+
+// searchFormat.addEventListener('change', '.dropdown-toggle', selectItem);
+
+searchBtn.addEventListener('click',function(event) {   
+    let searchParams = "search=" + searchString + "&format=" + searchFormat;
+    location.replace("./search-results.html?"+ searchParams);
+});
---
+++ assets/scripts/search-scripts.js
@@ -0,0 +1,8 @@
+let apiUrl = "https://www.loc.gov/";
+let searchP = document.location.search;
+let SFormat = searchP.split('=');
+
+function getLocData() {
+    fetch(apiURL + "search/")
+    
+}
